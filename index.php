1ea1538f46095003056a4b44f2c9822af9bc8da6
Wei Zhang <weizhang@weis-imac.gateway.sasktel.net>
2018-03-30 13:09:14 -0600

    add html form

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Search User</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Search Users</h1>
+        <form action="">
+            Search Users: <input type="text" class="form-control">
+        </form>
+        <p>Suggestion: <span id="output" style="font-weight:bold" ></span></p>
+    </div>
+    
+</body>
+</html>
