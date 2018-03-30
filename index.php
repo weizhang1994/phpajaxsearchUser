48a3158d244fecb310a6a5d7493bd4a3114d17d1
Wei Zhang <weizhang@weis-imac.gateway.sasktel.net>
2018-03-30 13:31:06 -0600

    add javascript to send request

--- index.php
+++ index.php
@@ -6,12 +6,30 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Search User</title>
     <link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css">
+    <script>
+        function showSuggestion(str){
+            if(str.length == 0){
+                document.getElementById('output').innerHTML = '';
+            } else {
+                // AJAX REQ
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function(){
+                    if(this.readyState == 4 && this.status == 200){
+                            document.getElementById('output').innerHTML = this.responseText;
+                                innerHTML = this.responseText;
+                    }
+                }
+                xmlhttp.open("GET", "suggest.php?q="+str, true);
+                xmlhttp.send();
+            }
+        }
+    </script>
 </head>
 <body>
     <div class="container">
         <h1>Search Users</h1>
         <form action="">
-            Search Users: <input type="text" class="form-control">
+            Search Users: <input type="text" class="form-control" onkeyup="showSuggestion(this.value)">
         </form>
         <p>Suggestion: <span id="output" style="font-weight:bold" ></span></p>
     </div>
